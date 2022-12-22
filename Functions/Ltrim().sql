75619736208df1a6ef9c6789a98ac82bda3be8fb
raresql <121085931+raresql@users.noreply.github.com>
2022-12-22 16:44:28 +0500

    Update Ltrim().sql

--- Functions/Ltrim().sql
+++ Functions/Ltrim().sql
@@ -5,10 +5,13 @@
 --Date Modified: 22 Dec 2022
 
 --Example-1
+
 --Declare variable
 DECLARE @EMAIL VARCHAR(100)
+
 --Set value of variable
 SET @EMAIL = '     KenSanchez@gmail.com';
+
 --Use LTRIM()
 SELECT  @EMAIL   AS [OriginalEmail]
 , LTRIM(@EMAIL)  AS [LTrimEmail];
@@ -18,12 +21,14 @@ GO
 
 --Enhancement
 --Pass the newly introduced parameter in LTRIM()
-
 --Example-2
+
 --Declare variable
 DECLARE @EMAIL VARCHAR(100) 
+
 --Set value of variable
 SET @EMAIL = '     ;KenSanchez@gmail.com';
+
 --Use LTRIM()
 SELECT  @EMAIL       AS [OriginalEmail]
 , LTRIM(@EMAIL,'; ')  AS [LTrimEmail];
@@ -32,10 +37,13 @@ GO
 --End of example-2
 
 --Example-3
+
 --Declare variable
 DECLARE @EMAIL VARCHAR(100)
+
 --Set value of variable
 SET @EMAIL = '     KenSanchez@gmail.com';
+
 --Use LTRIM()
 SELECT  @EMAIL   AS [OriginalEmail]
 , LTRIM(@EMAIL)  AS [LTrimEmail]
