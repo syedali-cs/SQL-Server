e45f02c162b3cf14d377e12b6cce28397dfd8e35
raresql <121085931+raresql@users.noreply.github.com>
2022-12-25 23:54:44 +0500

    Update TRANSLATE().sql

--- Functions/TRANSLATE().sql
+++ Functions/TRANSLATE().sql
@@ -1,10 +1,11 @@
--- Script Name: TRANSLATE()
--- Script Type: Function
--- Developed By: Muhammad Imran
--- Date Created: 05 Nov 2022
--- Date Modified: 22 Dec 2022
+--Script Name: TRANSLATE()
+--Script Type: Function
+--Article: https://raresql.com/2022/11/05/sql-server-string-functions-translate/
+--Developed By: Muhammad Imran
+--Date Created: 05 Nov 2022
+--Date Modified: 22 Dec 2022
 
--- Set Compatibility Level
+--Set Compatibility Level
 ALTER DATABASE Northwind SET COMPATIBILITY_LEVEL = 140;
 GO
 
@@ -14,27 +15,30 @@ DECLARE @Number VARCHAR(25)
 SET @Number='+92-3317892345'
 SELECT @Number AS PhoneNumber
      , REPLACE(REPLACE(@Number,'+',' '),'-',' ') AS ReplacedPhoneNumber;
- 
 GO 
 --End of example-1
 
 --Example-2
---New approach using TRANSLATE() function
+--New approach 
+--Declare variable
 DECLARE @Number AS VARCHAR(25)
+
+--Set varaiable
 SET @Number='+92-3317892345'
+
+--Use TRANSLATE() function
 SELECT @Number AS PhoneNumber
-     , TRANSLATE(@Number,'+-','  ') AS TranslatedPhoneNumber
- 
+     , TRANSLATE(@Number,'+-','  ') AS TranslatedPhoneNumber 
 GO
 --End of example-2
 
 --Example 3 
---Old approach using REPLACE() function
+--Old approach 
 --Use Northwind Database
 USE Northwind
 GO
 
---Select Statement 
+--Use REPLACE() function 
 SELECT FirstName
      , LastName
      , Title
@@ -49,7 +53,7 @@ GO
 USE Northwind
 GO
  
---Select Statement
+--Use TRANSLATE() function
 SELECT FirstName
      , LastName
      , Title 
