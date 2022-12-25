11fca3691bd0a7413f41e3a38cae640fa9557641
raresql <121085931+raresql@users.noreply.github.com>
2022-12-25 18:32:09 +0500

    Update LAG().sql

--- Functions/LAG().sql
+++ Functions/LAG().sql
@@ -1,21 +1,21 @@
 --Script Name: LAG()
 --Script Type: Function
+--Article: https://raresql.com/2012/11/21/sql-server-2012-analytic-functions-lag/
 --Developed By: Muhammad Imran
 --Date Created: 21 Nov 2012
 --Date Modified: 22 Dec 2022
 
 --Sample
 
---Use database tempdb
+--Use tempdb database
 USE tempdb
 GO
 
-
 --Create table
-Create Table [Test_Table]
+CREATE TABLE [Test_Table]
 (
- [S.No] int,
- [Letters] varchar(50)
+  [S.No] int
+, [Letters] varchar(50)
 )
 GO
 
@@ -35,38 +35,33 @@ Insert into [Test_Table] values (10,'LETTER I')
 --Example-1
 --Default values
 
---Use LAG()
-Select [S.No],[Letters]
-	 , LAG([Letters]) 
-	 Over (Order By [Letters]) 
-	 as [Previous Value]
-from  [Test_Table]
+--Use LAG() function 
+SELECT [S.No]
+     , [Letters]
+     , LAG([Letters]) OVER (ORDER BY [Letters]) AS [Previous Value]
+FROM  [Test_Table]
 
 --End of example-1
 
 --Example-2
 --Replace default values
 
---Use LAG()
-Select [S.No]
-	 , [Letters]
-	 , LAG([Letters],1,'') 
-	 Over (Order By [Letters]) 
-	 as [Previous Value]
-from  [Test_Table]
+--Use LAG() function
+SELECT [S.No]
+     , [Letters]
+     , LAG([Letters],1,'') OVER (ORDER BY [Letters]) AS [Previous Value]
+FROM [Test_Table]
 
 --End of example-2
 
 --Example-3
 --Change offset values
 
---Use LAG()
-Select [S.No]
-	 , [Letters]
-	 , LAG([Letters],2,'') 
-	 Over (Order By [Letters]) 
-	 as [Previous Value]
-from  [Test_Table]
+--Use LAG() function
+SELECT [S.No]
+     , [Letters]
+     , LAG([Letters],2,'') OVER (ORDER BY [Letters]) AS [Previous Value]
+FROM  [Test_Table]
 
 --End of example-3
 
@@ -78,12 +73,12 @@ Use tempdb
 GO
 
 --Create table
-Create table [Daily_Currency_Rate]
+CREATE TABLE [Daily_Currency_Rate]
 (
- [S.No] int,
- [Date] datetime,
- [Currency] varchar(50),
- [Exchange Rate] numeric(18,4)
+ [S.No] INT, 
+ [Date] DATETIME,
+ [Currency] VARCHAR(50),
+ [Exchange Rate] NUMERIC(18,4)
 )
  
 --Insert records into Daily_Currency_Rate
