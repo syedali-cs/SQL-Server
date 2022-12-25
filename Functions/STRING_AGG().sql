f69a6a7052b94bace47a6b440e54928ccf1470b3
raresql <121085931+raresql@users.noreply.github.com>
2022-12-25 18:47:31 +0500

    Update STRING_AGG().sql

--- Functions/STRING_AGG().sql
+++ Functions/STRING_AGG().sql
@@ -1,8 +1,9 @@
--- Script Name: STRING_AGG()
--- Script Type: Fuction
--- Developed By: Muhammad Imran
--- Date Created: 01 Dec 2022
--- Date Modified: 21 Dec 2022
+--Script Name: STRING_AGG()
+--Script Type: Fuction
+--Article: https://raresql.com/2022/12/01/sql-server-string_agg-function/
+--Developed By: Muhammad Imran
+--Date Created: 01 Dec 2022
+--Date Modified: 21 Dec 2022
 
 --Sample
 USE Northwind
@@ -16,9 +17,11 @@ GO
 -- End of Sample
 
 --Example-1
+--Use Northwind Database
 USE Northwind
 GO
  
+--Use STRING_AGG() function
 SELECT [Country]
      , STRING_AGG(CompanyName,', ') AS [CompanyName]
 FROM   [dbo].[Customers]
@@ -28,9 +31,11 @@ GO
 -- End of example-1
 
 --Example-2
+--Use Northwind Database
 USE Northwind
 GO
- 
+
+--Use STRING_AGG() function
 SELECT [Country]
        ,STRING_AGG(CompanyName,', ') WITHIN GROUP (ORDER BY CompanyName) AS [CompanyName]
 FROM   [dbo].[Customers]
@@ -40,10 +45,12 @@ GO
 -- End of example-2
 
 --Example-3
+--Use Northwind Database
 USE Northwind
 GO
- 
-SELECT [CompanyName] 
+
+--Use STRING_AGG() function 
+SELECT [CompanyName]
       , STRING_AGG(OrderID,',') AS OrderIDs
 FROM   [Customers]
 INNER JOIN [Orders] ON [Customers].[CustomerID]=[Orders].[CustomerID] 
