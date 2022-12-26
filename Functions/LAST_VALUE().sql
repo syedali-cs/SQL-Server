70279d1528cab73088f0c9b33d32e04d54781342
raresql <121085931+raresql@users.noreply.github.com>
2022-12-26 18:36:49 +0500

    Update LAST_VALUE().sql

--- Functions/LAST_VALUE().sql
+++ Functions/LAST_VALUE().sql
@@ -1,5 +1,6 @@
---Script Name: LAST_VALUE()
+--Script Name: LAST_VALUE() function
 --Script Type: Function
+--Article: https://raresql.com/2012/08/30/sql-server-2012-analytic-function-last_value/
 --Developed By: Muhammad Imran
 --Date Created: 30 Aug 2012
 --Date Modified: 23 Dec 2022
@@ -7,7 +8,7 @@
 --Sample
 
 --Create table
-Create Table [Student]
+CREATE Table [Student]
 	 (
       [Student ID] int,
 	  [Student Name] varchar(50),
@@ -17,38 +18,36 @@ Create Table [Student]
 GO
 
 --Insert records into table Student 
-Insert into [Student] values (3,'Derek','English',100)
-Insert into [Student] values (3,'Derek','Math',60)
-Insert into [Student] values (3,'Derek','Science',90)
- 
-Insert into [Student] values (1,'Bob','English',80)
-Insert into [Student] values (1,'Bob','Math',75)
-Insert into [Student] values (1,'Bob','Science',60)
- 
-Insert into [Student] values (2,'Sandra','English',70)
-Insert into [Student] values (2,'Sandra','Math',80)
-Insert into [Student] values (2,'Sandra','Science',40)
+INSERT INTO [Student] values (3, 'Derek', 'English',100)
+INSERT INTO [Student] values (3, 'Derek', 'Math',60)
+INSERT INTO [Student] values (3, 'Derek', 'Science',90)
+INSERT INTO [Student] values (1,'Bob','English',80)
+INSERT INTO [Student] values (1,'Bob','Math',75)
+INSERT INTO [Student] values (1,'Bob','Science',60)
+INSERT INTO [Student] values (2,'Sandra','English',70)
+INSERT INTO [Student] values (2,'Sandra','Math',80)
+INSERT INTO [Student] values (2,'Sandra','Science',40)
 
 --End of sample
 
 --Example-1
---alphabetical order
+--Alphabetical order
 
---Use LAST_VALUE function
-Select [Student ID]
+--Use LAST_VALUE() function
+SELECT [Student ID]
 	 , [Student Name]
 	 , [Subject]
 	 , [Marks]
 	 , LAST_Value([Student Name]) 
 	   Over (Order By [Student Name])
 	   as [Last Value]
-from dbo.[Student]
+FROM dbo.[Student]
 
 --Use CURRENT ROW
 --Use UNBOUNDED ROW
---Use LAST_VALUE function
+--Use LAST_VALUE() function
 
-Select [Student ID]
+SELECT [Student ID]
 	 , [Student Name]
 	 , [Subject]
 	 , [Marks]
@@ -56,7 +55,7 @@ Select [Student ID]
 	   Over (Order By [Student Name]
 	   ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) 
 	   as [Last Value]
-from dbo.[Student]
+FROM dbo.[Student]
 
 --End of example-1
 
@@ -65,15 +64,15 @@ from dbo.[Student]
 
 --Use CURRENT ROW
 --Use UNBOUNDED ROW
---Use LAST_VALUE function
-Select [Student ID]
+--Use LAST_VALUE() function
+SELECT [Student ID]
 	 , [Student Name]
 	 , [Subject]
 	 , [Marks]
 	 , LAST_Value([Student Name]) Over (Order By [Marks] Desc
 	   ROWS BETWEEN  CURRENT ROW AND UNBOUNDED FOLLOWING) 
 	   AS [LAST Value]
-from dbo.[Student]
+FROM dbo.[Student]
 
 --End of example-2
 
@@ -82,12 +81,12 @@ from dbo.[Student]
 
 --Use CURRENT ROW
 --Use UNBOUNDED ROW
---Use LAST_VALUE function
-Select [Student ID]
+--Use LAST_VALUE() function
+SELECT [Student ID]
 	 , [Student Name]
 	 , [Subject]
 	 , [Marks]
 	 , LAST_Value([Student Name]) Over (Partition By [Subject] Order By [Marks] Desc
 	   ROWS BETWEEN  CURRENT ROW AND UNBOUNDED FOLLOWING) 
 	   AS [LAST Value]
-from dbo.[Student]
+FROM dbo.[Student]
