da1efa2b7ed9acce06ffd66388b0d28adb3a5e47
raresql <121085931+raresql@users.noreply.github.com>
2022-12-26 18:37:30 +0500

    Update FIRST_VALUE().sql

--- Functions/FIRST_VALUE().sql
+++ Functions/FIRST_VALUE().sql
@@ -1,5 +1,6 @@
 --Script Name: FIRST_VALUE()
 --Script Type: Function
+--Article: https://raresql.com/2012/08/29/sql-server-2012-analytic-function-first_value/
 --Developed By: Muhammad Imran
 --Date Created: 27 Aug 2012
 --Date Modified: 23 Dec 2022
@@ -7,7 +8,7 @@
 --Sample
 
 --Create table
-Create Table [Student]
+CREATE Table [Student]
 	(
 	 [Student ID] int,
 	 [Student Name] varchar(50),
@@ -17,55 +18,53 @@ Create Table [Student]
 GO
 
 --Insert records into table Student
-Insert into [Student] values (3,'Derek','English',100)
-Insert into [Student] values (3,'Derek','Math',60)
-Insert into [Student] values (3,'Derek','Science',90)
- 
-Insert into [Student] values (1,'Bob','English',80)
-Insert into [Student] values (1,'Bob','Math',75)
-Insert into [Student] values (1,'Bob','Science',60)
- 
-Insert into [Student] values (2,'Sandra','English',70)
-Insert into [Student] values (2,'Sandra','Math',80)
-Insert into [Student] values (2,'Sandra','Science',40)
+INSERT INTO [Student] values (3,'Derek','English',100)
+INSERT INTO [Student] values (3,'Derek','Math',60)
+INSERT INTO [Student] values (3,'Derek','Science',90) 
+INSERT INTO [Student] values (1,'Bob','English',80)
+INSERT INTO [Student] values (1,'Bob','Math',75)
+INSERT INTO [Student] values (1,'Bob','Science',60)
+INSERT INTO [Student] values (2,'Sandra','English',70)
+INSERT INTO [Student] values (2,'Sandra','Math',80)
+INSERT INTO [Student] values (2,'Sandra','Science',40)
 
 --End of sample
 
 --Example-1
 --Ascending order
 
---Use FIRST_VALUE function
-Select [Student ID]
+--Use FIRST_VALUE() function
+SELECT [Student ID]
 	 , [Student Name]
 	 , [Subject]
 	 , [Marks]
 	 , FIRST_Value([Student Name]) 
 	   Over (Order By [Student Name]) 
 	   as [First Value]
-from dbo.[Student]
+FROM dbo.[Student]
 
 --End of example-1
 
 --Example-2
 --Top scorer
 
---Use FIRST_VALUE function
-Select [Student ID]
+--Use FIRST_VALUE() function
+SELECT [Student ID]
 	 , [Student Name]
 	 , [Subject]
 	 , [Marks]
 	 , FIRST_Value([Student Name]) 
 	   Over (Order By [Marks] Desc) 
 	   as [First Value]
-from dbo.[Student]
+FROM dbo.[Student]
 
 --End of example-2
 
 --Example-3
 --High score in each subject
 
---Use FIRST_VALUE function
-Select [Student ID]
+--Use FIRST_VALUE() function
+SELECT [Student ID]
 	 , [Student Name]
 	 , [Subject]
 	 , [Marks]
@@ -73,6 +72,6 @@ Select [Student ID]
 	   Over (Partition By [Subject] 
 	   Order By [Marks] Desc) 
 	   as [First Value]
-from dbo.[Student]
+FROM dbo.[Student]
 
 --End of example-3
